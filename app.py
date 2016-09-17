93a0ce5b78186dc23cf2114eca367ed5e4ab5b27
Robert Rees <shudderfix@gmail.com>
2016-09-17 14:30:54 +0100

    Inline the initial route but add a module for later

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ import flask
 
 app = flask.Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return "Hello world"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = int(os.environ.get('PORT', 3000)))
