32f5d544a417b3481434dbcde1beabd59d5fde41
Robert Rees <shudderfix@gmail.com>
2016-09-18 05:05:37 +0100

    Creates aa dummy callback route and login journey

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 import json
+import logging
 
 import requests
 import flask
+
+import auth0
 
 app = flask.Flask(__name__)
 
@@ -10,6 +13,21 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     return "Hello world"
+
+@app.route('/home')
+def home():
+	return "Home page"
+
+@app.route('/callback')
+def authorisation_callback():
+	code = flask.request.args.get('code')
+
+	user_info = auth0.obtain_user_info(code)
+
+	if user_info:
+		flask.session['profile'] = user_info
+
+	return flask.redirect('/home')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = int(os.environ.get('PORT', 3000)))
---
+++ auth0.py
@@ -0,0 +1,7 @@
+import os
+import logging
+
+import requests
+
+def obtain_user_info(code):
+	pass
