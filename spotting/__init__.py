82109590a37fbd89abe1504efecd413573534c5e
Robert Rees <shudderfix@gmail.com>
2016-09-26 13:12:23 +0100

    Creates a variable behaviour navigation bar, pushes logged in users to their home page and creates some editorconfig

--- spotting/__init__.py
+++ spotting/__init__.py
@@ -19,28 +19,30 @@ app.secret_key = os.urandom(24)
 
 
 if not ENV == "DEV":
-	sslify = SSLify(app)
+    sslify = SSLify(app)
 
 @app.route('/')
 def index():
-    return flask.render_template("index.html", config=config.config, callback_url = config.callback_url[ENV])
+    if 'profile' in flask.session:
+        return flask.redirect('/home')
+    return flask.render_template("index.html", app_config=config.config, callback_url = config.callback_url[ENV])
 
 @app.route('/home')
 @decorators.requires_auth
 def home():
-	user = flask.session['profile']
-	return flask.render_template("home.html", user=user)
+    user = flask.session['profile']
+    return flask.render_template("home.html", user=user)
 
 @app.route('/callback')
 def authorisation_callback():
-	code = flask.request.args.get('code')
+    code = flask.request.args.get('code')
 
-	user_info = authentication.obtain_user_info(code)
+    user_info = authentication.obtain_user_info(code)
 
-	if user_info:
-		flask.session['profile'] = user_info
+    if user_info:
+        flask.session['profile'] = user_info
 
-	return flask.redirect('/home')
+    return flask.redirect('/home')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = int(os.environ.get('PORT', 3000)))
