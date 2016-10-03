04252a69a65fe5c591c9e8fd1cc1d4e00e2017d0
Robert Rees <shudderfix@gmail.com>
2016-10-03 22:48:05 +0100

    Creates a repository to actually log information and backs it with a Fieldbook client

---
+++ repositories/logs.py
@@ -0,0 +1,24 @@
+import os
+import datetime
+import json
+import logging
+
+import fieldbook_py as fieldbook
+
+fieldbook_config = json.loads(os.environ['FIELDBOOK_CONFIG'])
+logging.info(fieldbook_config)
+fieldbook_api = fieldbook.FieldbookClient(fieldbook_config['API_KEY'], fieldbook_config['API_SECRET'], fieldbook_config['URL'])
+
+def save_log(brand, model=None):
+
+    iso_now = datetime.date.today().isoformat()
+
+    log_data = {
+        'date': iso_now,
+        'brand': brand,
+    }
+
+    if model:
+        log_data['model'] = model
+
+    return fieldbook_api.add_row('logs', log_data)
