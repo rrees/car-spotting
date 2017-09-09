ca0cb7ad7b37ac1a84acd6a2e5d4cd417082dd70
Robert Rees <shudderfix@gmail.com>
2017-09-09 19:08:10 +0100

    Switches to tabs

--- spotting/authentication.py
+++ spotting/authentication.py
@@ -10,33 +10,33 @@ ENV = os.environ.get('ENV', 'PROD')
 
 def obtain_user_info(code):
 
-	token_url ="https://{domain}/oauth/token".format(domain=config.auth0['domain'])
+    token_url ="https://{domain}/oauth/token".format(domain=config.auth0['domain'])
 
-	payload = {
-		'client_id': config.auth0['client_id'],
-		'client_secret': os.environ['AUTH0_SECRET'],
-		'redirect_uri': config.callback_url[ENV],
-		'code': code,
-		'grant_type': 'authorization_code',
-	}
+    payload = {
+        'client_id': config.auth0['client_id'],
+        'client_secret': os.environ['AUTH0_SECRET'],
+        'redirect_uri': config.callback_url[ENV],
+        'code': code,
+        'grant_type': 'authorization_code',
+    }
 
-	#logging.info(payload)
+    #logging.info(payload)
 
-	token_info_response = requests.post(token_url, data=json.dumps(payload), headers=headers.json)
+    token_info_response = requests.post(token_url, data=json.dumps(payload), headers=headers.json)
 
-	token_info_json = token_info_response.json()
+    token_info_json = token_info_response.json()
 
-	#logging.info(token_info_json)
+    #logging.info(token_info_json)
 
     if not 'access_token' in token_info_json:
         return None
 
-	user_url = "https://{domain}/userinfo?access_token={access_token}".format(domain=config.auth0['domain'],
-		access_token=token_info_json['access_token'])
+    user_url = "https://{domain}/userinfo?access_token={access_token}".format(domain=config.auth0['domain'],
+        access_token=token_info_json['access_token'])
 
-	user_info_response = requests.get(user_url)
-	user_info_json = user_info_response.json()
+    user_info_response = requests.get(user_url)
+    user_info_json = user_info_response.json()
 
-	#logging.info(user_info_json)
+    #logging.info(user_info_json)
 
-	return user_info_json
+    return user_info_json
