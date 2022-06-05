7899ea5090910194b247f075b4e01ab0672f8ead
Robert Rees <shudderfix@gmail.com>
2022-06-05 11:41:26 +0100

    Unpin dependencies from the Pipfile

--- spotting/auth_password/forms.py
+++ spotting/auth_password/forms.py
@@ -2,5 +2,5 @@ from wtforms import Form, validators
 from wtforms import fields
 
 class Login(Form):
-    email = fields.StringField('Username', [validators.required()])
-    password = fields.PasswordField('Password', [validators.required()])
+    email = fields.StringField('Username', [validators.InputRequired()])
+    password = fields.PasswordField('Password', [validators.InputRequired()])
